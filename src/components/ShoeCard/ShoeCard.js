3ee1e3e953e26b917f4bef31ec5c983cbdb307a1
Matheus Cabral <matheus.cabral@gmail.com>
2021-08-30 23:37:14 -0300

    ShoeCard

--- src/components/ShoeCard/ShoeCard.js
+++ src/components/ShoeCard/ShoeCard.js
@@ -40,22 +40,66 @@ const ShoeCard = ({
         <Spacer size={12} />
         <Row>
           <Name>{name}</Name>
-          <Price>{formatPrice(price)}</Price>
+          <Price style={{ '--text-decoration': salePrice ? 'line-through' : 'none'}}>{formatPrice(price)}</Price>
         </Row>
         <Row>
           <ColorInfo>{pluralize('Color', numOfColors)}</ColorInfo>
+          {salePrice && <SalePrice>{formatPrice(salePrice)}</SalePrice>}
         </Row>
+        <Banner variant={variant}>{variant}</Banner>
       </Wrapper>
     </Link>
   );
 };
+
+const Banner = ({ variant} ) => {
+  if (variant === 'on-sale') {
+    return <SaleBanner>Sale</SaleBanner>
+  }
+
+  if (variant === 'new-release') {
+    return <ReleaseBanner>Just Released!</ReleaseBanner>
+  }
+
+  return null;
+}
+
+const BannerBase = styled.div`
+   position: absolute;
+   top: 12px;
+   right: -4px;
+   border-radius: 2px;
+   padding: 8px;
+   font-weight: 700;
+   color: white;
+   font-size: ${14/16}rem;
+`;
+
+const SaleBanner = styled(BannerBase)`
+  background-color: #C5295D;
+`;
+
+const ReleaseBanner = styled(BannerBase)`
+  background-color: #6868D9;
+
+
+
+// const Banner = styled.div`
+//   position: absolute;
+//   top: 12px;
+//   right: -4px;
+//   background-color: red;
+//   padding: 8px;
+// `;
 
 const Link = styled.a`
   text-decoration: none;
   color: inherit;
 `;
 
-const Wrapper = styled.article``;
+const Wrapper = styled.article`
+  position: relative;
+`;
 
 const ImageWrapper = styled.div`
   position: relative;
@@ -67,6 +111,9 @@ const Image = styled.img`
 
 const Row = styled.div`
   font-size: 1rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: baseline;
 `;
 
 const Name = styled.h3`
@@ -74,7 +121,9 @@ const Name = styled.h3`
   color: ${COLORS.gray[900]};
 `;
 
-const Price = styled.span``;
+const Price = styled.span`
+  text-decoration: var(--text-decoration);
+`;
 
 const ColorInfo = styled.p`
   color: ${COLORS.gray[700]};
